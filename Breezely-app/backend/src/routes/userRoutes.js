0648f673eec533dc10c3a146b46d5ad25448db2d
sahan <edu.sahan@gmail.com>
2025-12-05 21:10:00 +0530

    create correct API endpoints.

--- Breezely-app/backend/src/routes/userRoutes.js
+++ Breezely-app/backend/src/routes/userRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+
+//import as a module
 const { registerUser } = require("../controllers/user.controller");
 
 router.post("/register", registerUser);
