3876fb5a34a33fb386b4468dee7f7563d097cdd9
sahan <edu.sahan@gmail.com>
2026-01-08 10:33:32 +0530

    Remove mobile package.json and tsconfig.json files from the Breezely-app repository

--- Breezely-app/mobile/app/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from 'expo-router';
-
-// This file is now ONLY used to redirect to the first screen you want to show.
-export default function StartupPage() {
-  // The Redirect component instantly navigates the user to the specified path.
-  // It is the first screen you see until a user is logged in.
-  return <Redirect href="/(auth)/register" />;
-}
